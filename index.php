dde37803afc7cb6496d27fd5eabd6da0fc4797d6
DockCapone1 <62236101+DockCapone1@users.noreply.github.com>
2020-03-25 22:29:51 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
-require_once ('functions.php');
+ require_once ('functions.php');
 
-$list_category =a;
+ $list_category =a;
 
-$list_name =b;
+ $list_name =b;
 
-$page_content =p;
-$page_content = conpile_template('index.php',
+ $page_content =p;
+ $lot_time_remaining =h;
+ $page_content = conpile_template('index.php',
     ['list_category'=> a,
-        'list_name' => b
+        'list_name' => b,
+        'lot_time_remaining' => h
     ]);
-$is_auth =c;
-$user_avatar =d;
-$user_name =i;
-$layout_content = conpile_template('layout.php', [
+ $is_auth =c;
+ $user_avatar =d;
+ $user_name =i;
+ $layout_content = conpile_template('layout.php',
+     [
     'page_title'=> 'Главная страница',
     'is_auth'=> c ,
     'user_avatar'=> d,
     'user_name'=> i,
     'page_content'=> p,
-    'list_category'=> a]);
+    'list_category'=> a
+     ]);
 
-print($layout_content);
+ print($layout_content);
+ ?>
 
-?>
